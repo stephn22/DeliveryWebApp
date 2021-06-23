f9c4282a87794ff8a031c424d5a714f041b45947
Stefano Crotti <20029190@studenti.uniupo.it>
2021-06-23 10:23:43 +0200

    Update StatusMessage with bootstrap

--- src/WebUI/Areas/Identity/Pages/Account/Manage/Request.cshtml.cs
+++ src/WebUI/Areas/Identity/Pages/Account/Manage/Request.cshtml.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
+using DeliveryWebApp.Domain.Constants;
 using DeliveryWebApp.Domain.Entities;
 using DeliveryWebApp.Infrastructure.Identity;
 using DeliveryWebApp.Infrastructure.Persistence;
@@ -23,22 +24,23 @@ namespace DeliveryWebApp.WebUI.Areas.Identity.Pages.Account.Manage
         private readonly UserManager<ApplicationUser> _userManager;
         private readonly ILogger<RequestModel> _logger;
         private readonly ApplicationDbContext _context;
+        private readonly SignInManager<ApplicationUser> _signInManager;
 
         public RequestModel(UserManager<ApplicationUser> userManager, ILogger<RequestModel> logger,
-            ApplicationDbContext context)
+            ApplicationDbContext context, SignInManager<ApplicationUser> signInManager)
         {
             _userManager = userManager;
             _logger = logger;
             _context = context;
+            _signInManager = signInManager;
         }
 
         public bool HasRequest { get; set; }
 
-        public Request Request { get; set; }
+        public Request UserRequest { get; set; }
+        public Client Client { get; set; }
 
         [BindProperty] public InputModel Input { get; set; }
-
-        [TempData] public string StatusMessage { get; set; }
 
         [BindProperty]
         public IEnumerable<SelectListItem> Roles => new[]
@@ -66,14 +68,14 @@ namespace DeliveryWebApp.WebUI.Areas.Identity.Pages.Account.Manage
         {
             try
             {
-                Request = await (from r in _context.Requests
+                UserRequest = await (from r in _context.Requests
                     where r.Client.ApplicationUserFk == user.Id
                     select r).FirstOrDefaultAsync();
             }
             catch (InvalidOperationException e)
             {
                 _logger.LogWarning($"{e.Message} - {nameof(Request)}: cannot resolve user request");
-                Request = null;
+                UserRequest = null;
             }
             finally
             {
@@ -92,7 +94,28 @@ namespace DeliveryWebApp.WebUI.Areas.Identity.Pages.Account.Manage
                 return Page();
             }
 
+            UserRequest = new Request
+            {
+                Client = await GetClientAsync(user),
+                Role = Input.Role,
+                Status = RequestStatus.Idle
+            };
+
+            _context.Requests.Add(UserRequest);
+
+            await _context.SaveChangesAsync();
+            await _signInManager.RefreshSignInAsync(user);
+
+            _logger.LogInformation("User posted their request successfully.");
+
             return RedirectToPage();
+        }
+
+        private async Task<Client> GetClientAsync(ApplicationUser user)
+        {
+            return await (from c in _context.Clients
+                where c.ApplicationUserFk == user.Id
+                select c).FirstOrDefaultAsync();
         }
     }
 }
