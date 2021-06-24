0d2ea45b4e3204bb6a278d408929c190b2e2aa14
Stefano Crotti <20029190@studenti.uniupo.it>
2021-06-24 12:11:40 +0200

    Add GetClientQuery

---
+++ src/Application/Clients/Queries/GetClients/GetClientQuery.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using DeliveryWebApp.Application.Common.Interfaces;
+using DeliveryWebApp.Domain.Entities;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+
+namespace DeliveryWebApp.Application.Clients.Queries.GetClients
+{
+    public static class GetClientQuery
+    {
+        /// <summary>
+        /// Get the Client instance given the identifier (Client.Id)
+        /// </summary>
+        /// <param name="id">Identifier of the client</param>
+        /// <returns>Client instance</returns>
+        public static async Task<Client> GetClientAsync(this IApplicationDbContext context, int? clientId)
+        {
+            try
+            {
+                if (clientId != null)
+                {
+                    return await (context.Clients.Where(c => c.Id == clientId)).FirstAsync();
+                }
+
+                throw new InvalidOperationException();
+            }
+            catch (InvalidOperationException e)
+            {
+                Console.WriteLine(e);
+                return null;
+            }
+        }
+    }
+
+}
