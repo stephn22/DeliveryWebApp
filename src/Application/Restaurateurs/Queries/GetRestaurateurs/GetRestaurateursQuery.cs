5c2a7643add4904abc2192b4fb7f0f642614b718
Stefano Crotti <20029190@studenti.uniupo.it>
2021-06-24 12:10:29 +0200

    Refactored Queries

---
+++ src/Application/Restaurateurs/Queries/GetRestaurateurs/GetRestaurateursQuery.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+using DeliveryWebApp.Application.Common.Interfaces;
+using DeliveryWebApp.Domain.Entities;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+
+namespace DeliveryWebApp.Application.Restaurateurs.Queries.GetRestaurateurs
+{
+    public class GetRestaurateursQuery : IRequest<List<Restaurateur>>
+    {
+    }
+
+    public class GetRestaurateursQueryHandler : IRequestHandler<GetRestaurateursQuery, List<Restaurateur>>
+    {
+        private readonly IApplicationDbContext _context;
+
+        public GetRestaurateursQueryHandler(IApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<List<Restaurateur>> Handle(GetRestaurateursQuery request, CancellationToken cancellationToken)
+        {
+            return await _context.Restaurateurs.ToListAsync(cancellationToken);
+        }
+    }
+}
---
+++ src/Application/Riders/Queries/GetRiders/GetRidersQuery.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+using DeliveryWebApp.Application.Common.Interfaces;
+using DeliveryWebApp.Domain.Entities;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+
+namespace DeliveryWebApp.Application.Riders.Queries.GetRiders
+{
+    public class GetRidersQuery : IRequest<List<Rider>>
+    {
+    }
+
+    public class GetRidersQueryHandler : IRequestHandler<GetRidersQuery, List<Rider>>
+    {
+        private readonly IApplicationDbContext _context;
+
+        public GetRidersQueryHandler(IApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<List<Rider>> Handle(GetRidersQuery request, CancellationToken cancellationToken)
+        {
+            return await _context.Riders.ToListAsync(cancellationToken);
+        }
+    }
+}
