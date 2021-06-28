8babc221e888c7eaeef3d1c2617012a9688ac277
Stefano Crotti <20029190@studenti.uniupo.it>
2021-06-28 17:58:13 +0200

    Add CreateAddressCommand

---
+++ src/Application/Addresses/Commands/CreateAddress/CreateAddressCommand.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using DeliveryWebApp.Application.Common.Interfaces;
+using DeliveryWebApp.Domain.Entities;
+using MediatR;
+
+namespace DeliveryWebApp.Application.Addresses.Commands.CreateAddress
+{
+    public class CreateAddressCommand : IRequest<int>
+    {
+        public string AddressLine1 { get; set; }
+        public string AddressLine2 { get; set; }
+        public string Number { get; set; }
+        public string City { get; set; }
+        public string PostalCode { get; set; }
+        public string Country { get; set; }
+    }
+
+    public class CreateAddressCommandHandler : IRequestHandler<CreateAddressCommand, int>
+    {
+        private readonly IApplicationDbContext _context;
+
+        public CreateAddressCommandHandler(IApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<int> Handle(CreateAddressCommand request, CancellationToken cancellationToken)
+        {
+            var entity = new Address
+            {
+                AddressLine1 = request.AddressLine1,
+                AddressLine2 = request.AddressLine1,
+                City = request.City,
+                Country = request.Country,
+                Number = request.Number,
+                PostalCode = request.PostalCode
+            };
+
+            _context.Addresses.Add(entity);
+
+            await _context.SaveChangesAsync(cancellationToken);
+
+            return entity.Id;
+        }
+    }
+}
