a0cbe2c53b24a21081c355e2a18e985fd95f6523
Stefano Crotti <20029190@studenti.uniupo.it>
2021-07-01 14:10:58 +0200

    Update MappingTests

--- tests/Application.UnitTests/Common/Mappings/MappingTests.cs
+++ tests/Application.UnitTests/Common/Mappings/MappingTests.cs
@@ -1,51 +1,48 @@
-﻿//using AutoMapper;
-//using DeliveryWebApp.Application.Common.Mappings;
-//using DeliveryWebApp.Application.TodoLists.Queries.GetTodos;
-//using DeliveryWebApp.Domain.Entities;
-//using NUnit.Framework;
-//using System;
-//using System.Runtime.Serialization;
+﻿using AutoMapper;
+using DeliveryWebApp.Application.Common.Mappings;
+using DeliveryWebApp.Domain.Entities;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Runtime.Serialization;
 
-//namespace DeliveryWebApp.Application.UnitTests.Common.Mappings
-//{
-//    public class MappingTests
-//    {
-//        private readonly IConfigurationProvider _configuration;
-//        private readonly IMapper _mapper;
+namespace DeliveryWebApp.Application.UnitTests.Common.Mappings
+{
+    public class MappingTests
+    {
+        private readonly IConfigurationProvider _configuration;
+        private readonly IMapper _mapper;
 
-//        public MappingTests()
-//        {
-//            _configuration = new MapperConfiguration(cfg =>
-//            {
-//                cfg.AddProfile<MappingProfile>();
-//            });
+        public MappingTests()
+        {
+            _configuration = new MapperConfiguration(cfg => { cfg.AddProfile<MappingProfile>(); });
 
-//            _mapper = _configuration.CreateMapper();
-//        }
+            _mapper = _configuration.CreateMapper();
+        }
 
-//        [Test]
-//        public void ShouldHaveValidConfiguration()
-//        {
-//            _configuration.AssertConfigurationIsValid();
-//        }
-        
-//        //[Test]
-//        //[TestCase(typeof(TodoList), typeof(TodoListDto))]
-//        //[TestCase(typeof(TodoItem), typeof(TodoItemDto))]
-//        public void ShouldSupportMappingFromSourceToDestination(Type source, Type destination)
-//        {
-//            var instance = GetInstanceOf(source);
+        [Test]
+        public void ShouldHaveValidConfiguration()
+        {
+            _configuration.AssertConfigurationIsValid();
+        }
 
-//            _mapper.Map(instance, source, destination);
-//        }
+        [Test]
+        [TestCase(typeof(Product), typeof(Product))]
+        [TestCase(typeof(Product), typeof(Product))]
+        public void ShouldSupportMappingFromSourceToDestination(Type source, Type destination)
+        {
+            var instance = GetInstanceOf(source);
 
-//        private object GetInstanceOf(Type type)
-//        {
-//            if (type.GetConstructor(Type.EmptyTypes) != null)
-//                return Activator.CreateInstance(type);
+            _mapper.Map(instance, source, destination);
+        }
 
-//            // Type without parameterless constructor
-//            return FormatterServices.GetUninitializedObject(type);
-//        }
-//    }
-//}
+        private object GetInstanceOf(Type type)
+        {
+            return type.GetConstructor(Type.EmptyTypes) != null
+                ? Activator.CreateInstance(type)
+                : FormatterServices.GetUninitializedObject(type);
+
+            // Type without parameterless constructor
+        }
+    }
+}
