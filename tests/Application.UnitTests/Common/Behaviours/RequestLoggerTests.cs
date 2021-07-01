e80d7a25771df40bbbd297b0339d4e81a91d656a
Stefano Crotti <20029190@studenti.uniupo.it>
2021-07-01 13:24:42 +0200

    Update requestlogger tests

--- tests/Application.UnitTests/Common/Behaviours/RequestLoggerTests.cs
+++ tests/Application.UnitTests/Common/Behaviours/RequestLoggerTests.cs
@@ -1,50 +1,52 @@
-﻿//using DeliveryWebApp.Application.Common.Behaviours;
-//using DeliveryWebApp.Application.Common.Interfaces;
-//using DeliveryWebApp.Application.TodoItems.Commands.CreateTodoItem;
-//using Microsoft.Extensions.Logging;
-//using Moq;
-//using NUnit.Framework;
-//using System.Threading;
-//using System.Threading.Tasks;
+﻿using DeliveryWebApp.Application.Common.Behaviours;
+using DeliveryWebApp.Application.Common.Interfaces;
+using DeliveryWebApp.Application.Customers.Commands.CreateCustomer;
+using Microsoft.Extensions.Logging;
+using Moq;
+using NUnit.Framework;
+using System.Threading;
+using System.Threading.Tasks;
 
-//namespace DeliveryWebApp.Application.UnitTests.Common.Behaviours
-//{
-//    public class RequestLoggerTests
-//    {
-//        private readonly Mock<ILogger<CreateTodoItemCommand>> _logger;
-//        private readonly Mock<ICurrentUserService> _currentUserService;
-//        private readonly Mock<IIdentityService> _identityService;
+namespace DeliveryWebApp.Application.UnitTests.Common.Behaviours
+{
+    public class RequestLoggerTests
+    {
+        private readonly Mock<ILogger<CreateCustomerCommand>> _logger;
+        private readonly Mock<ICurrentUserService> _currentUserService;
+        private readonly Mock<IIdentityService> _identityService;
+
+        private const string TestApplicationUserFk = "b3ececd-4ragFfgk";
 
 
-//        public RequestLoggerTests()
-//        {
-//            _logger = new Mock<ILogger<CreateTodoItemCommand>>();
+        public RequestLoggerTests()
+        {
+            _logger = new Mock<ILogger<CreateCustomerCommand>>();
 
-//            _currentUserService = new Mock<ICurrentUserService>();
+            _currentUserService = new Mock<ICurrentUserService>();
 
-//            _identityService = new Mock<IIdentityService>();
-//        }
+            _identityService = new Mock<IIdentityService>();
+        }
 
-//        [Test]
-//        public async Task ShouldCallGetUserNameAsyncOnceIfAuthenticated()
-//        {
-//            _currentUserService.Setup(x => x.UserId).Returns("Administrator");
+        [Test]
+        public async Task ShouldCallGetUserNameAsyncOnceIfAuthenticated()
+        {
+            _currentUserService.Setup(x => x.UserId).Returns("Administrator");
 
-//            var requestLogger = new LoggingBehaviour<CreateTodoItemCommand>(_logger.Object, _currentUserService.Object, _identityService.Object);
+            var requestLogger = new LoggingBehaviour<CreateCustomerCommand>(_logger.Object, _currentUserService.Object, _identityService.Object);
 
-//            await requestLogger.Process(new CreateTodoItemCommand { ListId = 1, Title = "title" }, new CancellationToken());
+            await requestLogger.Process(new CreateCustomerCommand() { ApplicationUserFk = TestApplicationUserFk }, new CancellationToken());
 
-//            _identityService.Verify(i => i.GetUserNameAsync(It.IsAny<string>()), Times.Once);
-//        }
+            _identityService.Verify(i => i.GetUserNameAsync(It.IsAny<string>()), Times.Once);
+        }
 
-//        [Test]
-//        public async Task ShouldNotCallGetUserNameAsyncOnceIfUnauthenticated()
-//        {
-//            var requestLogger = new LoggingBehaviour<CreateTodoItemCommand>(_logger.Object, _currentUserService.Object, _identityService.Object);
+        [Test]
+        public async Task ShouldNotCallGetUserNameAsyncOnceIfUnauthenticated()
+        {
+            var requestLogger = new LoggingBehaviour<CreateCustomerCommand>(_logger.Object, _currentUserService.Object, _identityService.Object);
 
-//            await requestLogger.Process(new CreateTodoItemCommand { ListId = 1, Title = "title" }, new CancellationToken());
+            await requestLogger.Process(new CreateCustomerCommand { ApplicationUserFk = TestApplicationUserFk }, new CancellationToken());
 
-//            _identityService.Verify(i => i.GetUserNameAsync(null), Times.Never);
-//        }
-//    }
-//}
+            _identityService.Verify(i => i.GetUserNameAsync(null), Times.Never);
+        }
+    }
+}
