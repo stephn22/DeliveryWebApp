88dc8cc5957593d9709244030329ed9c2f2f8714
Stefano Crotti <20029190@studenti.uniupo.it>
2021-07-01 23:08:38 +0200

    Update Queries and commands in application

---
+++ src/Application/Products/Queries/GetProducts/GetProductsQuery.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using DeliveryWebApp.Application.Common.Exceptions;
+using DeliveryWebApp.Application.Common.Interfaces;
+using DeliveryWebApp.Domain.Entities;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+
+namespace DeliveryWebApp.Application.Products.Queries.GetProducts
+{
+    public class GetProductsQuery : IRequest<List<Product>>
+    {
+        public int? RestaurantId { get; set; }
+        public int? OrderId { get; set; }
+    }
+
+    public class GetProductsQueryHandler : IRequestHandler<GetProductsQuery, List<Product>>
+    {
+        private readonly IApplicationDbContext _context;
+
+        public GetProductsQueryHandler(IApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<List<Product>> Handle(GetProductsQuery request, CancellationToken cancellationToken)
+        {
+            if (request.RestaurantId != null)
+            {
+                return await (from r in _context.Restaurants
+                    where r.Id == request.RestaurantId
+                    select r.Products.ToList()).FirstAsync(cancellationToken); // TODO: check
+            }
+
+            if (request.OrderId != null)
+            {
+                return await (from o in _context.Orders
+                    where o.Id == request.OrderId
+                    select o.Products.ToList()).FirstAsync(cancellationToken); // TODO: check
+            }
+
+            throw new NotFoundException();
+        }
+    }
+}
--- src/Application/Restaurants/Commands/UpdateRestaurant/UpdateRestaurantCommand.cs
+++ src/Application/Restaurants/Commands/UpdateRestaurant/UpdateRestaurantCommand.cs
@@ -14,6 +14,7 @@ namespace DeliveryWebApp.Application.Restaurants.Commands.UpdateRestaurant
     public class UpdateRestaurantCommand : IRequest
     {
         public int Id { get; set; }
+        public byte[] Logo { get; set; }
         public string Name { get; set; }
         public string Category { get; set; }
         public Address Address { get; set; }
@@ -35,6 +36,11 @@ namespace DeliveryWebApp.Application.Restaurants.Commands.UpdateRestaurant
             if (entity == null)
             {
                 throw new NotFoundException(nameof(Restaurant), request.Id);
+            }
+
+            if (request.Logo != null)
+            {
+                entity.Logo = request.Logo;
             }
 
             if (request.Name != null)
