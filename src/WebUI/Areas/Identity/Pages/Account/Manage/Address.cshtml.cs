1023f1e926c5feb81af10e31e8feed63d957d04e
Stefano Crotti <20029190@studenti.uniupo.it>
2021-07-02 10:20:50 +0200

    Update Address identity page

--- src/WebUI/Areas/Identity/Pages/Account/Manage/Address.cshtml.cs
+++ src/WebUI/Areas/Identity/Pages/Account/Manage/Address.cshtml.cs
@@ -1,20 +1,24 @@
+using DeliveryWebApp.Application.Addresses.Queries.GetAddresses;
 using DeliveryWebApp.Domain.Entities;
 using DeliveryWebApp.Infrastructure.Identity;
 using DeliveryWebApp.Infrastructure.Persistence;
 using DeliveryWebApp.Infrastructure.Security;
 using DeliveryWebApp.Infrastructure.Services.Utilities;
-using IdentityServer4.Extensions;
+using MediatR;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Logging;
-using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
+using DeliveryWebApp.Application.Addresses.Commands.CreateAddress;
+using DeliveryWebApp.Application.Addresses.Commands.UpdateAddress;
+using DeliveryWebApp.Application.Customers.Commands.UpdateCustomer;
 
 namespace DeliveryWebApp.WebUI.Areas.Identity.Pages.Account.Manage
 {
@@ -24,25 +28,25 @@ namespace DeliveryWebApp.WebUI.Areas.Identity.Pages.Account.Manage
         private readonly UserManager<ApplicationUser> _userManager;
         private readonly ILogger<AddressModel> _logger;
         private readonly ApplicationDbContext _context;
+        private readonly IMediator _mediator;
 
         public AddressModel(UserManager<ApplicationUser> userManager, ILogger<AddressModel> logger,
-            ApplicationDbContext context)
+            ApplicationDbContext context, IMediator mediator)
         {
             _userManager = userManager;
             _logger = logger;
             _context = context;
+            _mediator = mediator;
         }
 
         [TempData] public string StatusMessage { get; set; }
 
         [BindProperty] public InputModel Input { get; set; }
 
-        public IQueryable<ICollection<Address>> Addresses { get; set; }
+        public List<Address> Addresses { get; set; }
 
         public SelectList Countries => new(Utilities.CountryList(), "Key", "Value");
         public bool HasAtLeastOneAddress { get; set; }
-        public bool Edit { get; set; }
-        public bool AddNew { get; set; }
 
         public class InputModel
         {
@@ -72,7 +76,6 @@ namespace DeliveryWebApp.WebUI.Areas.Identity.Pages.Account.Manage
 
             [Required]
             [DataType(DataType.Text)]
-            [Display(Name = "State or Province")]
             public string Country { get; set; }
         }
 
@@ -81,48 +84,99 @@ namespace DeliveryWebApp.WebUI.Areas.Identity.Pages.Account.Manage
             var user = await _userManager.GetUserAsync(User);
             if (user == null)
             {
-                return NotFound($"Unable to load user with ID '{_userManager.GetUserId(User)}'.");
+                _logger.LogError($"Unable to load user with ID '{_userManager.GetUserId(User)}'.");
+                return NotFound();
             }
 
-            LoadAddresses(user);
+            await LoadAddressesAsync(user);
             return Page();
         }
 
+        public async Task<IActionResult> OnPostUpdateAddressAsync(int id)
+        {
+            var user = await _userManager.GetUserAsync(User);
+            if (user == null)
+            {
+                _logger.LogError($"Unable to load user with ID '{_userManager.GetUserId(User)}'.");
+                return NotFound();
+            }
+
+            if (!ModelState.IsValid)
+            {
+                await LoadAddressesAsync(user);
+                return Page();
+            }
+
+            await _mediator.Send(new UpdateAddressCommand
+            {
+                Id = id,
+                AddressLine1 = Input.AddressLine1,
+                AddressLine2 = Input.AddressLine2,
+                City = Input.City,
+                Country = Input.Country,
+                Number = Input.Number,
+                PostalCode = Input.PostalCode
+            });
+
+            _logger.LogInformation($"Updated address with id '{id}'.");
+            StatusMessage = "Your address has been updated";
+
+            return RedirectToPage();
+        }
+
+        /// <summary>
+        /// Called when customer add new address for the first time
+        /// </summary>
+        /// <returns></returns>
         public async Task<IActionResult> OnPostAsync()
         {
             var user = await _userManager.GetUserAsync(User);
             if (user == null)
             {
-                return NotFound($"Unable to load user with ID '{_userManager.GetUserId(User)}'.");
+                _logger.LogError($"Unable to load user with ID '{_userManager.GetUserId(User)}'.");
+                return NotFound();
             }
 
             if (!ModelState.IsValid)
             {
-                LoadAddresses(user);
+                await LoadAddressesAsync(user);
                 return Page();
             }
 
-            StatusMessage = "Your addresses have been updated";
+            var customer = await (from c in _context.Customers
+                where c.ApplicationUserFk == user.Id
+                select c).FirstAsync();
+
+            var address = new Address
+            {
+                AddressLine1 = Input.AddressLine1,
+                AddressLine2 = Input.AddressLine2,
+                City = Input.City,
+                Country = Input.Country,
+                Number = Input.Number,
+                PostalCode = Input.PostalCode
+            };
+
+            await _mediator.Send(new UpdateCustomerCommand
+            {
+                Id = customer.Id,
+                Address = address
+            });
+
+            StatusMessage = "Your address has been updated";
             return RedirectToPage();
         }
 
-        private void LoadAddresses(ApplicationUser user)
+        private async Task LoadAddressesAsync(ApplicationUser user)
         {
-            try
+            var customer = await (from c in _context.Customers
+                                   where c.ApplicationUserFk == user.Id
+                                   select c).FirstAsync();
+
+            Addresses = await _mediator.Send(new GetAddressesQuery
             {
-                Addresses = from c in _context.Customers
-                            where user.Id == c.ApplicationUserFk
-                            select c.Addresses;
-            }
-            catch (InvalidOperationException e)
-            {
-                _logger.LogWarning($"{e.Message} - {nameof(Addresses)}: cannot resolve user addresses");
-                Addresses = null;
-            }
-            finally
-            {
-                HasAtLeastOneAddress = (!Addresses.IsNullOrEmpty());
-            }
+                CustomerId = customer.Id
+            });
         }
     }
 }
