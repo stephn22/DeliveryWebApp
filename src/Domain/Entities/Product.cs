138b9736a577ab643eaa486f80b1c2f32045ca59
Stefano Crotti <20029190@studenti.uniupo.it>
2021-07-02 17:52:13 +0200

    Update Product entity and add product tests

--- src/Domain/Entities/Product.cs
+++ src/Domain/Entities/Product.cs
@@ -14,5 +14,21 @@ namespace DeliveryWebApp.Domain.Entities
         public int Discount { get; set; }
         public string Category { get; set; }
         public int Quantity { get; set; }
+
+        public double ApplyDiscount()
+        {
+            if (Discount != 0)
+            {
+                return Price * ((100.00 - Discount) / 100.00);
+            }
+
+            return Price;
+        }
+
+        public static double TotalPrice(List<Product> list)
+        {
+            const double tot = 0.00;
+            return list.Count <= 0 ? tot : Math.Round(list.Sum(p => (p.ApplyDiscount() * p.Quantity)), 2);
+        }
     }
 }
---
+++ tests/Domain.UnitTests/Product/ProductTests.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Xunit;
+
+
+namespace DeliveryWebApp.Domain.UnitTests.Product
+{
+    public class ProductTests
+    {
+
+        [Fact]
+        public void Discount()
+        {
+            var p1 = new Entities.Product {Price = 22.50, Discount = 10};
+            var p2 = new Entities.Product {Price = 25.50, Discount = 0};
+
+            var finalPrice = p1.ApplyDiscount();
+            var noDiscount = p2.ApplyDiscount();
+
+            Assert.Equal(20.25, finalPrice);
+            Assert.Equal(25.50, noDiscount);
+        }
+
+        [Fact]
+        public void TotalPriceWithDiscount()
+        {
+            var list = new List<Entities.Product>
+            {
+                new() {Price = 25.30, Quantity = 37, Discount = 30},
+                new() {Price = 11.75, Quantity = 21, Discount = 20},
+                new() {Price = 21.30, Quantity = 11, Discount = 15},
+                new() {Price = 35.25, Quantity = 16, Discount = 0}
+            };
+
+            var tot = Entities.Product.TotalPrice(list);
+
+            Assert.Equal(1615.82, tot);
+        }
+    }
+}
