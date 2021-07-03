5224e20d37e48b835d4589c56be6a64f6733e46f
Stefano Crotti <20029190@studenti.uniupo.it>
2021-07-03 17:45:08 +0200

    Add GetRestaurantsQuery

---
+++ src/Application/Restaurants/Queries/GetRestaurants/GetRestaurantsQuery.cs
@@ -0,0 +1,29 @@
+﻿using DeliveryWebApp.Application.Common.Interfaces;
+using DeliveryWebApp.Domain.Entities;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace DeliveryWebApp.Application.Restaurants.Queries.GetRestaurants
+{
+    public class GetRestaurantsQuery : IRequest<List<Restaurant>>
+    {
+    }
+
+    public class GetRestaurantsQueryHandler : IRequestHandler<GetRestaurantsQuery, List<Restaurant>>
+    {
+        private readonly IApplicationDbContext _context;
+
+        public GetRestaurantsQueryHandler(IApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<List<Restaurant>> Handle(GetRestaurantsQuery request, CancellationToken cancellationToken)
+        {
+            return await _context.Restaurants.ToListAsync(cancellationToken);
+        }
+    }
+}
