714212b7b460dd5827c680269b6bcf15e7990e07
Stefano Crotti <20029190@studenti.uniupo.it>
2021-07-03 12:43:34 +0200

    Add ProductCategory constants

---
+++ src/Domain/Constants/ProductCategory.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DeliveryWebApp.Domain.Constants
+{
+    public static class ProductCategory
+    {
+        public const string Unassigned = "Unassigned";
+    }
+}
