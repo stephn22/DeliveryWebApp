12622de0681e2c2e1bcaab96b10b53beeff2363c
Stefano Crotti <20029190@studenti.uniupo.it>
2021-07-04 09:46:25 +0200

    Add migration UpdateAddress

---
+++ src/WebUI/Migrations/20210703201706_UpdateAddress.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace DeliveryWebApp.WebUI.Migrations
+{
+    public partial class UpdateAddress : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Addresses_Customers_CustomerId",
+                table: "Addresses");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "CustomerId",
+                table: "Addresses",
+                type: "int",
+                nullable: false,
+                defaultValue: 0,
+                oldClrType: typeof(int),
+                oldType: "int",
+                oldNullable: true);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Addresses_Customers_CustomerId",
+                table: "Addresses",
+                column: "CustomerId",
+                principalTable: "Customers",
+                principalColumn: "Id");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Addresses_Customers_CustomerId",
+                table: "Addresses");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "CustomerId",
+                table: "Addresses",
+                type: "int",
+                nullable: true,
+                oldClrType: typeof(int),
+                oldType: "int");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Addresses_Customers_CustomerId",
+                table: "Addresses",
+                column: "CustomerId",
+                principalTable: "Customers",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+        }
+    }
+}
