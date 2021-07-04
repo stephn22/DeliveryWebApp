377633140d8365d27f757c589cbe3e821535b78d
Stefano Crotti <20029190@studenti.uniupo.it>
2021-07-04 09:46:41 +0200

    Update DeleteCustomerCommand

--- src/Application/Customers/Commands/DeleteCustomer/DeleteCustomerCommand.cs
+++ src/Application/Customers/Commands/DeleteCustomer/DeleteCustomerCommand.cs
@@ -1,13 +1,11 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading;
-using System.Threading.Tasks;
-using DeliveryWebApp.Application.Common.Exceptions;
+﻿using DeliveryWebApp.Application.Common.Exceptions;
 using DeliveryWebApp.Application.Common.Interfaces;
 using DeliveryWebApp.Domain.Entities;
 using MediatR;
+using Microsoft.EntityFrameworkCore;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
 
 namespace DeliveryWebApp.Application.Customers.Commands.DeleteCustomer
 {
@@ -34,6 +32,10 @@ namespace DeliveryWebApp.Application.Customers.Commands.DeleteCustomer
                 throw new NotFoundException(nameof(Customer), request.Id);
             }
 
+            var addresses = await _context.Addresses.Where(a => a.CustomerId == entity.Id)
+                .ToListAsync(cancellationToken);
+
+            _context.Addresses.RemoveRange(addresses);
             _context.Customers.Remove(entity);
 
             await _context.SaveChangesAsync(cancellationToken);
