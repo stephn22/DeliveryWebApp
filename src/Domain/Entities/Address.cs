efc694f02ab752e845b500a32fbf6c7fe4a8c46f
Stefano Crotti <20029190@studenti.uniupo.it>
2021-07-04 09:45:47 +0200

    Update Address entity

--- src/Domain/Entities/Address.cs
+++ src/Domain/Entities/Address.cs
@@ -14,6 +14,8 @@ namespace DeliveryWebApp.Domain.Entities
         public string City { get; set; }
         public string PostalCode { get; set; }
         public string Country { get; set; }
+        public int CustomerId { get; set; }
+        public Customer Customer { get; set; }
 
         public override string ToString()
         {
