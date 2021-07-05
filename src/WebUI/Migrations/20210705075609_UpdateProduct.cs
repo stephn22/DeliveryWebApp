14c7e94f05385fa2902390008948ffa957c2926b
Stefano Crotti <20029190@studenti.uniupo.it>
2021-07-05 10:30:04 +0200

    Add UpdateProduct migration

---
+++ src/WebUI/Migrations/20210705075609_UpdateProduct.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace DeliveryWebApp.WebUI.Migrations
+{
+    public partial class UpdateProduct : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Products_Restaurants_RestaurantId",
+                table: "Products");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "RestaurantId",
+                table: "Products",
+                type: "int",
+                nullable: false,
+                defaultValue: 0,
+                oldClrType: typeof(int),
+                oldType: "int",
+                oldNullable: true);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "DeliveryDate",
+                table: "Orders",
+                type: "datetime2",
+                nullable: true);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Products_Restaurants_RestaurantId",
+                table: "Products",
+                column: "RestaurantId",
+                principalTable: "Restaurants",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Products_Restaurants_RestaurantId",
+                table: "Products");
+
+            migrationBuilder.DropColumn(
+                name: "DeliveryDate",
+                table: "Orders");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "RestaurantId",
+                table: "Products",
+                type: "int",
+                nullable: true,
+                oldClrType: typeof(int),
+                oldType: "int");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Products_Restaurants_RestaurantId",
+                table: "Products",
+                column: "RestaurantId",
+                principalTable: "Restaurants",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+        }
+    }
+}
