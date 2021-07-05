4f6ed5c55169692514039f82aa8f86710ccc7daf
Stefano Crotti <20029190@studenti.uniupo.it>
2021-07-05 15:02:48 +0200

    Add migrations

--- src/WebUI/Migrations/20210705125747_UpdateCustomer.Designer.cs
+++ src/WebUI/Migrations/20210705125747_UpdateCustomer.Designer.cs
@@ -4,14 +4,16 @@ using DeliveryWebApp.Infrastructure.Persistence;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace DeliveryWebApp.WebUI.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20210705125747_UpdateCustomer")]
+    partial class UpdateCustomer
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
