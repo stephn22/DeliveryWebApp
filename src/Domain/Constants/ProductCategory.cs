e6feca9b6ae1effacb0f2f27d32446302c4acfb5
Stefano Crotti <20029190@studenti.uniupo.it>
2021-07-07 17:14:51 +0200

    Update ProductCategory

--- src/Domain/Constants/ProductCategory.cs
+++ src/Domain/Constants/ProductCategory.cs
@@ -9,5 +9,13 @@ namespace DeliveryWebApp.Domain.Constants
     public static class ProductCategory
     {
         public const string Unassigned = "Unassigned";
+        public const string Hamburger = "Hamburger";
+        public const string Sushi = "Sushi";
+        public const string Pizza = "Pizza";
+        public const string Drink = "Drink";
+        public const string Dessert = "Dessert";
+        public const string Vegan = "Vegan";
+        public const string Chicken = "Chicken";
+        public const string Fish = "Fish";
     }
 }
