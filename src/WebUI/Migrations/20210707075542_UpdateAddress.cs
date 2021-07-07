b2bf8a232133838b465fecf07c0f134f2f91d24b
Stefano Crotti <20029190@studenti.uniupo.it>
2021-07-07 10:18:48 +0200

    Add migrations

---
+++ src/WebUI/Migrations/20210707075542_UpdateAddress.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace DeliveryWebApp.WebUI.Migrations
+{
+    public partial class UpdateAddress : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<double>(
+                name: "Latitude",
+                table: "Addresses",
+                type: "float",
+                nullable: false,
+                defaultValue: 0.0);
+
+            migrationBuilder.AddColumn<double>(
+                name: "Longitude",
+                table: "Addresses",
+                type: "float",
+                nullable: false,
+                defaultValue: 0.0);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Latitude",
+                table: "Addresses");
+
+            migrationBuilder.DropColumn(
+                name: "Longitude",
+                table: "Addresses");
+        }
+    }
+}
