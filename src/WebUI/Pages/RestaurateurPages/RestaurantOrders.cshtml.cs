7b7e8ca1d3bbaed53b869058c0c0eb271c9d9ce0
Stefano Crotti <20029190@studenti.uniupo.it>
2021-08-04 16:59:13 +0200

    Using html+bootstrap tables for master branch

--- src/WebUI/Pages/RestaurateurPages/RestaurantOrders.cshtml.cs
+++ src/WebUI/Pages/RestaurateurPages/RestaurantOrders.cshtml.cs
@@ -1,3 +1,10 @@
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using DeliveryWebApp.Application.Common.Interfaces;
+using DeliveryWebApp.Application.Orders.Queries.GetOrders;
+using DeliveryWebApp.Application.Restaurants.Queries.GetRestaurants;
+using DeliveryWebApp.Domain.Entities;
+using MediatR;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
@@ -5,16 +12,36 @@ namespace DeliveryWebApp.WebUI.Pages.RestaurateurPages
 {
     public class RestaurantOrdersModel : PageModel
     {
+        private readonly IMediator _mediator;
+        private readonly IApplicationDbContext _context;
+
+        public RestaurantOrdersModel(IMediator mediator, IApplicationDbContext context)
+        {
+            _mediator = mediator;
+            _context = context;
+        }
+
         public int Id { get; set; }
 
-        public IActionResult OnGet(int? id)
+        public Restaurant Restaurant { get; set; }
+
+        public List<Order> Orders { get; set; }
+
+        public async Task<IActionResult> OnGet(int? id)
         {
             if (id == null)
             {
-                return NotFound($"Could not find restaurant with id {id}");
+                return NotFound($"Could not find restaurant with this id");
             }
 
             Id = (int)id;
+
+            Restaurant = await _context.Restaurants.FindAsync(Id);
+
+            Orders = await _mediator.Send(new GetOrdersQuery
+            {
+                RestaurantId = Id
+            });
 
             return Page();
         }
--- src/WebUI/Pages/RestaurateurPages/RestaurantProducts.cshtml.cs
+++ src/WebUI/Pages/RestaurateurPages/RestaurantProducts.cshtml.cs
@@ -1,5 +1,10 @@
+using System.Collections.Generic;
+using System.Threading.Tasks;
 using DeliveryWebApp.Application.Common.Security;
+using DeliveryWebApp.Application.Products.Queries.GetProducts;
+using DeliveryWebApp.Domain.Entities;
 using DeliveryWebApp.Infrastructure.Security;
+using MediatR;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
@@ -8,16 +13,30 @@ namespace DeliveryWebApp.WebUI.Pages.RestaurateurPages
     [Authorize(Roles = RoleName.Restaurateur)]
     public class RestaurantProductsModel : PageModel
     {
+        private readonly IMediator _mediator;
+
+        public RestaurantProductsModel(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+
         public int Id { get; set; }
 
-        public IActionResult OnGet(int? id)
+        public List<Product> Products { get; set; }
+
+        public async Task<IActionResult> OnGet(int? id)
         {
             if (id == null)
             {
-                return NotFound($"Could not find restaurant with id {id}");
+                return NotFound($"Could not find restaurant with this id");
             }
 
             Id = (int)id;
+
+            Products = await _mediator.Send(new GetProductsQuery
+            {
+                RestaurantId = Id
+            });
 
             return Page();
         }
