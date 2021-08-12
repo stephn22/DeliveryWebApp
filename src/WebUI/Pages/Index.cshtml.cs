9e216aaa98cb98d160ee2b7931d8f6eba9ae41b7
Stefano Crotti <20029190@studenti.uniupo.it>
2021-08-12 17:58:55 +0200

    Update Index

--- src/WebUI/Pages/Index.cshtml.cs
+++ src/WebUI/Pages/Index.cshtml.cs
@@ -1,51 +1,20 @@
-﻿using DeliveryWebApp.Domain.Entities;
-using DeliveryWebApp.Infrastructure.Identity;
-using DeliveryWebApp.Infrastructure.Persistence;
-using IdentityServer4.Extensions;
-using Microsoft.AspNetCore.Identity;
-using Microsoft.AspNetCore.Mvc;
-using Microsoft.AspNetCore.Mvc.RazorPages;
-using Microsoft.EntityFrameworkCore;
+﻿using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.Extensions.Logging;
-using System.Threading.Tasks;
 
 namespace DeliveryWebApp.WebUI.Pages
 {
     public class IndexModel : PageModel
     {
         private readonly ILogger<IndexModel> _logger;
-        private readonly UserManager<ApplicationUser> _userManager;
-        private readonly ApplicationDbContext _context;
 
-        public IndexModel(ILogger<IndexModel> logger, UserManager<ApplicationUser> userManager, ApplicationDbContext context)
+        public IndexModel(ILogger<IndexModel> logger)
         {
             _logger = logger;
-            _userManager = userManager;
-            _context = context;
         }
 
-        public Customer Customer { get; set; }
-
-        public async Task<IActionResult> OnGetAsync()
+        public void OnGet()
         {
-            if (HttpContext.User.IsAuthenticated())
-            {
-                var user = await _userManager.GetUserAsync(User);
 
-                if (user == null)
-                {
-                    return NotFound();
-                }
-
-                await LoadCustomerAsync(user);
-            }
-
-            return Page();
-        }
-
-        private async Task LoadCustomerAsync(ApplicationUser user)
-        {
-            Customer = await _context.Customers.FirstAsync(c => c.ApplicationUserFk == user.Id);
         }
     }
 }
