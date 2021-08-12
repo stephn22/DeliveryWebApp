c63ac34af90a52924bdb7f7a737c0b8601c7e06a
Stefano Crotti <20029190@studenti.uniupo.it>
2021-08-12 11:14:57 +0200

    Update CreateRequestCommand

--- src/Application/Requests/Commands/CreateRequest/CreateRequestCommand.cs
+++ src/Application/Requests/Commands/CreateRequest/CreateRequestCommand.cs
@@ -1,9 +1,12 @@
-﻿using DeliveryWebApp.Application.Common.Interfaces;
+﻿using System;
+using DeliveryWebApp.Application.Common.Interfaces;
 using DeliveryWebApp.Domain.Entities;
 using MediatR;
 using System.Threading;
 using System.Threading.Tasks;
 using AutoMapper;
+using DeliveryWebApp.Application.Requests.Commands.DeleteRequest;
+using Microsoft.EntityFrameworkCore;
 
 namespace DeliveryWebApp.Application.Requests.Commands.CreateRequest
 {
@@ -18,19 +21,35 @@ namespace DeliveryWebApp.Application.Requests.Commands.CreateRequest
     {
         private readonly IApplicationDbContext _context;
         private readonly IMapper _mapper;
+        private readonly IMediator _mediator;
 
-        public CreateRequestCommandHandler(IApplicationDbContext context, IMapper mapper)
+        public CreateRequestCommandHandler(IApplicationDbContext context, IMapper mapper, IMediator mediator)
         {
             _context = context;
             _mapper = mapper;
+            _mediator = mediator;
         }
 
         public async Task<Request> Handle(CreateRequestCommand request, CancellationToken cancellationToken)
         {
             var entity = _mapper.Map<Request>(request);
 
-            _context.Requests.Add(entity);
+            // if customer has an old request, delete it
+            try
+            {
+                var oldRequest =
+                    await _context.Requests.FirstAsync(r => r.CustomerId == request.CustomerId, cancellationToken);
 
+                await _mediator.Send(new DeleteRequestCommand
+                {
+                    Id = oldRequest.Id
+                }, cancellationToken);
+            }
+            catch (InvalidOperationException)
+            {
+            }
+            
+            _context.Requests.Add(entity);
             await _context.SaveChangesAsync(cancellationToken);
 
             return entity;
