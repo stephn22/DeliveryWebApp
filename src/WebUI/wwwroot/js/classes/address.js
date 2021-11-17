8351011564116edab976acf529f4d1723b11da7e
Stefano Crotti <51045661+stecrotti1@users.noreply.github.com>
2021-11-17 16:46:14 +0100

    Delete useless files

--- src/WebUI/wwwroot/js/classes/address.js
+++
@@ -1,9 +0,0 @@
-﻿class Address {
-    constructor(placeName, longitude, latitude, customerId, restaurateurId) {
-        this.placeName = placeName;
-        this.longitude = longitude;
-        this.latitude = latitude;
-        this.customerId = customerId;
-        this.restaurateurId = restaurateurId;
-    }
-}
--- src/WebUI/wwwroot/js/classes/order.js
+++
@@ -1,9 +0,0 @@
-﻿class Order {
-    constructor(id, date, restaurateurId, status, deliveryAddressId) {
-        this.id = id;
-        this.date = date;
-        this.restaurateurId = restaurateurId;
-        this.status = status;
-        this.deliveryAddressId = deliveryAddressId;
-    }
-}
